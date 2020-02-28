346f602f2429c0484a9bb21668f9b4b696235d85
Catalin Ion <catalin@fastmail.com>
2020-02-28 17:16:40 +0200

    wip

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const { createMacro } = require('babel-plugin-macros');
+const { createMacro, MacroError } = require('babel-plugin-macros');
 
-function assertMacro({ references, state, babel }) {
-  const { default: defaultImport = [] } = references;
+module.exports = createMacro(debugMacro);
+
+function debugMacro({ references, state, babel }) {
+  const { assert = [], expose = [], default: defaultImport = [] } = references;
   const t = babel.types;
 
-  defaultImport.forEach(referencePath => {
+  assert.forEach(function asserMacro(referencePath) {
+    if (process.env.NODE_ENV === 'development') {
+      referencePath.parentPath.replaceWithMultiple([]);
+      return;
+    }
+
     if (referencePath.parentPath.type !== 'CallExpression') {
       throw new MacroError('assert.macro is a function(condition, message)');
     }
 
+    if (defaultImport.length) {
+      throw new MacroError('debug.macro has no default export');
+    }
+
     const args = referencePath.parentPath.get('arguments');
 
-    //referencePath.parentPath.replaceWithMultiple([]);
     referencePath.parentPath.replaceWithMultiple([
       t.ifStatement(
         t.unaryExpression('!', args[0].node),
@@ -25,8 +35,33 @@ function assertMacro({ references, state, babel }) {
       )
     ]);
   });
+
+  // expose.forEach(function exposeMacro(referencePath) {
+  //   if (process.env.NODE_ENV === 'development') {
+  //     return;
+  //   }
+  //
+  //   if (referencePath.parentPath.type !== 'CallExpression') {
+  //     throw new MacroError('expose.macro is a function(condition, message)');
+  //   }
+  //
+  //   const args = referencePath.parentPath.get('arguments');
+  //
+  //   referencePath.parentPath.replaceWithMultiple([
+  //     t.ifStatement(
+  //       t.unaryExpression('!', args[0].node),
+  //       t.blockStatement([
+  //         t.throwStatement(
+  //           t.newExpression(t.identifier('Error'), [
+  //             t.binaryExpression('+', t.stringLiteral('[Expose Failed]: '), args[1].node)
+  //           ])
+  //         )
+  //       ])
+  //     )
+  //   ]);
+  // });
+
+  return {
+    keepImports: false
+  };
 }
-
-const assert = createMacro(assertMacro);
-
-module.exports = assert;
